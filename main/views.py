0444ed0fbe1a16e945a781dfa0d189d7b0e814d8
pranj39 <pranjalshrestha336@gmail.com>
2025-04-21 10:19:36 +0545

    added templates

---
+++ main/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader, Template
+
+def home(request):
+    t = loader.get_template('home.html')
+    return HttpResponse(t.render())
+# Create your views here.
